1e3404a3e5f72f21e3d8fa2d814d1585579941d4
OnRaptor <raptorahead@gmail.com>
2022-12-02 18:09:59 +0500

    fix intersection bug

--- src/pages/Recs.jsx
+++ src/pages/Recs.jsx
@@ -6,6 +6,7 @@ import CircularProgress from "../components/ui/CircularProgress";
 import Text from "../components/ui/Text";
 import {useDispatch, useSelector} from "react-redux";
 import {setPage} from "../store/slices/PageSlice";
+import {FloatContainer} from "../components/ui/containers/FloatContainer";
 
 const RecsViewer = ({recs}) => {
     return (
@@ -21,15 +22,20 @@ const Recs = () => {
     const page = useSelector(state => state.pageSlice.page)
     const dispatch = useDispatch()
     const { data, isLoading } = useGetGenericDataQuery([GetDataType.Legends, page === 1 ? '' : page])
-    const [parsedData, setData] = useState(null)
+    const [parsedData, setData] = useState({ photos: null })
     const [error, setError] = useState()
     const lastElement = useRef()
     const observer = useRef()
 
     useEffect(()=>{
         if (data) {
+            console.log(parsedData)
+            console.log(data)
             if (typeof data === typeof {})
-                setData([...data.photos, ...data.photos])
+                if (parsedData.photos === null)
+                    setData([...data.photos])
+                else
+                    setData([...parsedData, ...data.photos])
             else
             {
                 if (data.data === GetDataErrorResult.serverError)
@@ -43,29 +49,33 @@ const Recs = () => {
     useEffect(()=>{
         if (isLoading) return;
         const callback = (entries, observer) =>{
-            console.log(isLoading)
             if (isLoading) return;
             if (entries[0].isIntersecting){
-                dispatch(setPage(1 + page))
+                console.log('callback with page:')
                 console.log(page)
-                console.log(parsedData)
+                dispatch(setPage(1 + page))
             }
         }
-        observer.current = new IntersectionObserver(callback)
-    }, [isLoading])
+        setTimeout(()=>{
+            observer.current = new IntersectionObserver(callback)
+            observer.current.observe(lastElement.current)
+        }, 1000)
+    })
 
     return (
         <ContentWrapper>
-            {!parsedData ?
+            {parsedData.photos === null ?
                 <CircularProgress/> :
                 <>
                     <RecsViewer recs={parsedData}/>
-                    {()=>observer.current.observe(lastElement.current)
-                    }
                     <div ref={lastElement} style={{width:"100%", height: '20px', background:'red'}}></div>
                 </>
             }
             {error && <Text color="red">{error}</Text>}
+
+            <FloatContainer justify="end">
+                <Text fontsize='23px'>{page}</Text>
+            </FloatContainer>
         </ContentWrapper>
     );
 };
